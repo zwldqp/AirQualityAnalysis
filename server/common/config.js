fd52a0742860e8a82c6bd84ccd36a5bbc2ab332a
bohexigua <aa869410917bb@qq.com>
2018-05-05 09:18:47 +0800

    增加后端

---
+++ server/common/config.js
@@ -0,0 +1,11 @@
+const localServer = "127.0.0.1";
+const databaseName = "nefu_zhangqu";
+const databaseUser = "root";
+const databasePassword = "aixiaozi1";
+
+module.exports = {
+  localServer: localServer,
+  databaseName: databaseName,
+  databaseUser: databaseUser,
+  databasePassword: databasePassword
+}
